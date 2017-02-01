28e74ff7877690f02da56dcb7f01ef6f74a575cf
Abylay Ospan <aospan@netup.ru>
2017-02-01 00:05:29 -0500

    OpenWRT build added
    Signed-off-by: Abylay Ospan <aospan@netup.ru>

--- README.md
+++ README.md
@@ -12,4 +12,10 @@ bcm4708-edgecore-ecw7220-l.dtb  linux-stable                    uImage
 
 folder 'linux-stable' is a full Linux kernel tree used for compilation.
 
-
+#building under OSx
+```
+hdiutil create -size 20g -fs "Case-sensitive HFS+" -volname OpenWrt OpenWrt.dmg
+hdiutil attach OpenWrt.dmg
+cd /Volumes/OpenWrt
+docker run -v `pwd`/opt:/opt -it ocp
+```
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,23 @@ set -e
 
 cd /opt
 
+# Build OpenWRT
+if [ ! -d "openwrt" ]; then
+	git clone --depth=1 https://github.com/aospan/openwrt.git
+fi
+
+cd openwrt
+
+if [ ! -f ".config" ]; then
+	cp /openwrt.config ./.config
+	FORCE=1 make defconfig
+fi
+
+FORCE_UNSAFE_CONFIGURE=1 FORCE=1 make -j"$(nproc)"
+PATH=$PATH:./staging_dir/host/bin/ ./scripts/ubinize-image.sh ./build_dir/target-arm_cortex-a9_musl-1.1.16_eabi/linux-bcm53xx/root.squashfs /opt/squashfs.ubi -p 128KiB -m 2048 -E 5
+
+cd /opt
+# Build Linux kernel
 if [ ! -d "linux-stable" ]; then
     git clone --depth=1 -b ocp-linux-4.9.y https://github.com/aospan/linux-stable.git
 fi
@@ -10,11 +27,11 @@ fi
 cd linux-stable 
 
 if [ ! -f ".config" ]; then
-    make -j 2 LOADADDR=0x82008000 ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- ecw7220l_defconfig
+	make -j"$(nproc)" LOADADDR=0x82008000 ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- ecw7220l_defconfig
 fi
 
-time make -j 2 LOADADDR=0x82008000 ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- uImage
-make -j 2 LOADADDR=0x82008000 ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- arch/arm/boot/dts/
+time make -j"$(nproc)" LOADADDR=0x82008000 ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- uImage
+make -j"$(nproc)" LOADADDR=0x82008000 ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- arch/arm/boot/dts/
 
 #copy result to /opt
 cp arch/arm/boot/uImage /opt/
